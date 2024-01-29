d543f6f3c306c985fafa2cad45026b893106118d
Breanna Young <w0459556@campus.nscc.ca>
2024-01-29 12:36:49 -0400

    fix

--- src/Food.java
+++ src/Food.java
@@ -1,2 +1,20 @@
 public class Food {
+    String foodName;
+    String foodType;
+
+    // constructor
+    public Food(String foodName, String foodType){
+        this.foodName = foodName;
+        this.foodType = foodType;
+    }
+
+    // functions
+    public void eatIt(){
+        System.out.println("you eat the " + foodName);
+    }
+
+    public void denyIt(){
+        System.out.println("you deny the " + foodName);
+    }
+
 }
